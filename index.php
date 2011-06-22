51525447b98683de5dfaa88aca8ccf940a43a3db
Octav Sandulescu <github@narc.ro>
2011-06-22 17:27:53 +0300

    Define some useful paths, and DO include adodb

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 error_reporting(E_ALL);
 ob_start();
 
-include('inc/conf.inc');
+define('PATH_BASE',  dirname(__FILE__));
+define('PATH_INC',   PATH_BASE.'/inc');
+define('PATH_ADODB', PATH_INC.'/adodb5');
+
+include(PATH_INC.'/conf.inc');
+include(PATH_ADODB.'/adodb.inc.php');
 
 $operation = '';
 if(!empty($_GET['args']))
